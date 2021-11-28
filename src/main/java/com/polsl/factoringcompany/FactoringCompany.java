19d90867947f51a0c6d611a29869715df00128dc
michalgorul <michalgorul@gmail.com>
2021-11-28 18:15:37 +0100

    update cors

--- src/main/java/com/polsl/factoringcompany/FactoringCompany.java
+++ src/main/java/com/polsl/factoringcompany/FactoringCompany.java
@@ -22,41 +22,6 @@ public class FactoringCompany {
         SpringApplication.run(FactoringCompany.class, args);
     }
 
-//    @Bean
-//    public ServletWebServerFactory servletContainer() {
-//        // Enable SSL Trafic
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//
-//        // Add HTTP to HTTPS redirect
-//        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-//
-//        return tomcat;
-//    }
-
-    /*
-    We need to redirect from HTTP to HTTPS. Without SSL, this application used
-    port 8080. With SSL it will use port 8443. So, any request for 8082 needs to be
-    redirected to HTTPS on 8443.
-     */
-//    private Connector httpToHttpsRedirectConnector() {
-//        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-//        connector.setScheme("http");
-//        connector.setPort(8081);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(8443);
-//        return connector;
-//    }
-
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
