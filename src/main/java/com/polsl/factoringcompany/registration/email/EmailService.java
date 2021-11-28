4063c124f45522a20d7926d04ba82cae9ec97805
michalgorul <michalgorul@gmail.com>
2021-11-28 22:25:23 +0100

    update smtp

--- src/main/java/com/polsl/factoringcompany/registration/email/EmailService.java
+++ src/main/java/com/polsl/factoringcompany/registration/email/EmailService.java
@@ -28,7 +28,7 @@ public class EmailService implements EmailSender {
             helper.setText(email, true);
             helper.setTo(to);
             helper.setSubject("Confirm your email");
-            helper.setFrom("factoring-company");
+            helper.setFrom("factoring-company@fc.com");
             mailSender.send(mimeMessage);
         } catch (MessagingException e) {
             LOGGER.error("failed to send email", e);
